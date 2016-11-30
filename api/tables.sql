44cc9d686ce2fbce2c1df0201b012252ea07b8d3
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-11-30 22:17:59 +0800

    added Userinfo table in tables.sql

--- api/tables.sql
+++ api/tables.sql
@@ -3,3 +3,17 @@ create table Roles
 	id 				SERIAL8 PRIMARY KEY,
 	role_name		VARCHAR(30) NOT NULL
 );
+
+create table Userinfo
+(
+	id				SERIAL8 PRIMARY KEY,
+	fname			VARCHAR(50),
+	mname			VARCHAR(50),
+	lname			VARCHAR(50),
+	address			VARCHAR(100) NOT NULL,
+	email			VARCHAR(50),
+	mobile_number	INT,
+	password		VARCHAR(50),
+	role_id			INT REFERENCES Roles(id),
+	earned_points	INT
+);
