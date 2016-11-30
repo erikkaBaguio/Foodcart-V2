e1a832ee658edd717e5a53c0c6912d55435c8d36
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-01 01:33:44 +0800

    modified __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-app.debug = True
+# app.debug = True
 
-# if __name__ == '__main__':
-#     app.run(debug=True,host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
 
 from app import views
