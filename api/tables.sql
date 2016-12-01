64ba576d32ddc8dff0fc25bd19220d682750c04d
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-01 08:32:03 +0800

    Added Restaurant table in tables.sql

--- api/tables.sql
+++ api/tables.sql
@@ -12,3 +12,12 @@ CREATE TABLE Category
 	category_name	TEXT NOT NULL,
 	is_active       BOOLEAN DEFAULT TRUE
 );
+
+CREATE TABLE Restaurant
+(
+	id           	SERIAL8 PRIMARY KEY,
+	resto_name 		TEXT NOT NULL,
+	min_order		FLOAT,
+	delivery_fee	FLOAT,
+  	is_active       BOOLEAN DEFAULT TRUE
+);
